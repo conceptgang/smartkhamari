ab67410dc234efaa1ba2c56e97220b3f0cd691b6
nasrin131 <nasrinakterlida@gmail.com>
2020-06-21 02:25:50 +0600

    The Expense page is done.

---
+++ component/src/main/java/com/conceptgang/component/ExpensePage.java
@@ -0,0 +1,14 @@
+package com.conceptgang.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpensePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_page);
+    }
+}
