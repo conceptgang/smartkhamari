4b80dd885fd6a3f79be4e2013735082e44117e0f
nasrin131 <nasrinakterlida@gmail.com>
2020-06-21 03:58:01 +0600

    The Siemens page is done.

---
+++ component/src/main/java/com/conceptgang/component/Siemens.java
@@ -0,0 +1,14 @@
+package com.conceptgang.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Siemens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siemens);
+    }
+}
