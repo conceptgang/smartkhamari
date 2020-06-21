f193b81604e3041bf2dfb9cd0387023cdc09280a
nasrin131 <nasrinakterlida@gmail.com>
2020-06-21 20:29:27 +0600

    The HR Cost page is done.

---
+++ component/src/main/java/com/conceptgang/component/HrCost.java
@@ -0,0 +1,14 @@
+package com.conceptgang.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HrCost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hr_cost);
+    }
+}
