2bd5d3b31aa3bba5e44b2e3270283305674b8539
zoha131 <abirhasanzoha@gmail.com>
2020-06-30 20:04:48 +0600

    side nav completed

--- app/src/main/java/com/conceptgang/app/MainActivity.kt
+++ app/src/main/java/com/conceptgang/app/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.conceptgang.app
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.conceptgang.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        //window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+
+        //window?.statusBarColor = getThemeColor(R.attr.colorSurface)
+        //window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;//  set status text dark
+        window.statusBarColor = ContextCompat.getColor(this, R.color.surface);// set status background white
+
     }
 }
